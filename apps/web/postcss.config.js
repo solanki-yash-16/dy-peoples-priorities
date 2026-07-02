3aa1429e4c87f3573d77b1710d7428646034da31
solanki-yash-16 <yashsolanki0777@gmail.com>
2026-07-02 23:37:18 +0530

    fix: The package installation for pnpm.

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,10 +1,3 @@
 export default {
-  plugins: {
-    "@tailwindcss/postcss": {
-      sources: [
-        "src/**/*.{js,jsx,ts,tsx}",
-        "../../packages/ui/src/**/*.{ts,tsx}"
-      ]
-    },
-  },
+  plugins: {},
 };
